86c5334ebf15af5ab1f351a1661b99600980c3f5
Deku-shrub <Deku-shrub@users.noreply.github.com>
2022-07-14 18:33:32 +0100

    Update requester.py

--- core/requester.py
+++ core/requester.py
@@ -1,4 +1,5 @@
 import requests
 
 def requester(url):
+	sleep 10 # limited to one request per 10 seconds alas https://www.blockchain.com/api/q
 	return requests.get('https://blockchain.info/rawaddr/' + url).text
